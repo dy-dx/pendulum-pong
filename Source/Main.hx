f8ef38bd5a192523236357944b59853d0ba640b2
Chris K <christopherjkim@gmail.com>
2015-04-02 16:08:00 -0400

    initialer commit

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,19 +1,33 @@
 package;
 
-
 import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.Lib;
+import openfl.display.FPS;
 
 
 class Main extends Sprite {
-	
-	
-	public function new () {
-		
-		super ();
-		
-		
-		
-	}
-	
-	
+
+  function init () {
+    // code
+  }
+
+  public function new () {
+    super();
+    addEventListener(Event.ADDED_TO_STAGE, added);
+  }
+
+  function added (e) {
+    removeEventListener(Event.ADDED_TO_STAGE, added);
+    init();
+  }
+
+  public static function main () {
+    // static entry point
+    Lib.current.stage.align = flash.display.StageAlign.TOP_LEFT;
+    Lib.current.stage.scaleMode = flash.display.StageScaleMode.NO_SCALE;
+    Lib.current.addChild(new Main());
+  }
+
+
 }
